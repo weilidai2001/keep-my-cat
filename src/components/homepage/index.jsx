3a651abf8aa9837f0b02cab9bc825853ce1cc74a
Weili Dai <weili.dai@turner.com>
2017-06-10 17:02:26 +0100

    Added file to /build for dynamic referencing

--- src/components/homepage/index.jsx
+++ src/components/homepage/index.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
-import placeholder from '../../img/placeholder-1.jpg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-    return (
-        <div className="page">
-            <div className="page-content">
-                <header>Header</header>
-                <main>
-                    <img className="hero-image" src={placeholder} alt=""/>
-                </main>
-                <footer>Footer</footer>
+
+class Homepage extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            heroImageUrl: '/placeholder-1.jpg'
+        };
+
+        this.onChoice1Click = this.onChoice1Click.bind(this);
+        this.onChoice2Click = this.onChoice2Click.bind(this);
+    }
+
+    onChoice1Click() {
+        console.log('Choice 1 clicked');
+        this.setState({
+            heroImageUrl: '/placeholder-2.png'
+        })
+    }
+
+    onChoice2Click() {
+
+    }
+
+    render() {
+        return (
+            <div className="page">
+                <div className="page-content">
+                    <header>
+                        <div className="next-options">
+                            <button onClick={this.onChoice1Click}>Next</button>
+                        </div>
+                    </header>
+                    <main>
+                        <img className="hero-image" src={this.state.heroImageUrl} alt=""/>
+                    </main>
+                    <footer>Footer</footer>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default Homepage;
