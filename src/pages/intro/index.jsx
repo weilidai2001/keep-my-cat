99df3e128fd68edb013fbc8d9e296561754e649f
Weili Dai <weili.dai@turner.com>
2017-07-10 22:25:19 +0100

    iPhone not displaying animation on first load

--- src/pages/intro/index.jsx
+++ src/pages/intro/index.jsx
@@ -5,25 +5,36 @@ class Intro extends Component {
 
     render() {
         const images = [
-            '/intro_1.gif',
-            '/intro_2.gif',
-            '/intro_3.gif',
-            '/intro_4.gif',
-            '/intro_5.gif',
-            '/intro_6.gif',
-            '/intro_7.gif',
-            '/intro_8.gif',
-            '/intro_9.gif',
-            '/intro_10.gif',
-            '/intro_10a.jpg',
-            '/intro_11.gif'
+            {src: '/intro_1.gif', cacheable: true},
+            {src: '/intro_2.gif', cacheable: false},
+            {src: '/intro_3.gif', cacheable: false},
+            {src: '/intro_4.gif', cacheable: false},
+            {src: '/intro_5.gif', cacheable: false},
+            {src: '/intro_6.gif', cacheable: false},
+            {src: '/intro_7.gif', cacheable: false},
+            {src: '/intro_8.gif', cacheable: false},
+            {src: '/intro_9.gif', cacheable: true},
+            {src: '/intro_10.gif', cacheable: false},
+            {src: '/intro_10a.jpg', cacheable: true},
+            {src: '/intro_11.gif', cacheable: true},
         ];
+
+        const rand = Math.random();
+        const cacheBustingImages = images.map(image => image.cacheable ? image.src : `${image.src}?${rand}`);
+
+        const preLoadImages = (images) => {
+            return () => images.forEach(img => {
+                const newImage = new Image();
+                newImage.src = img;
+            })
+        };
+
+        setImmediate(preLoadImages(cacheBustingImages));
 
         const nextDestination = '/dashboards/0/1';
         const buttonText = 'Skip intro';
 
-        const props = { images, nextDestination, buttonText };
-
+        const props = { images: cacheBustingImages, nextDestination, buttonText };
 
         return (
             <div className="page">
