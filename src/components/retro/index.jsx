11b1745c141a18792f177cbc56ec1c8545cc4aaa
Weili Dai <weili.dai@ebay.com>
2017-07-11 17:19:57 +0100

    Style Retro page

--- src/components/retro/index.jsx
+++ src/components/retro/index.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import {getRetroScript, getCurrentHeroImage} from '../../data/states';
 
 const styles = {
     retro: {
     },
+    missionsContainer: {
+        width: '100%',
+        paddingBottom: '66.666%',
+        position: 'relative'
+    },
     missions: {
+        position: 'absolute',
         display: 'flex',
+        width: '100%',
+        height: '100%',
         flexWrap: 'wrap'
     },
     mission: {
         width: '33.333%',
-        height: '33.333%'
+        height: '50%',
+        position: 'relative'
+    },
+    text: {
+        paddingTop: '15px',
+        textAlign: 'center',
+        margin: '0 auto',
+        width: '310px',
+        fontSize: '24px',
+        lineHeight: '1.3'
+    },
+    footer: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    button: {
+        fontSize: '15px',
+        width: 'auto',
+        padding: '5px 15px'
     }
 };
 
@@ -22,10 +49,15 @@ class Retro extends React.Component {
         };
 
         this.onMissionClick = this.onMissionClick.bind(this);
+        this.onReplayClick = this.onReplayClick.bind(this);
     }
 
     onMissionClick(script) {
         return () => this.setState({script});
+    }
+
+    onReplayClick() {
+        this.props.history.push('/');
     }
 
     render() {
@@ -35,27 +67,32 @@ class Retro extends React.Component {
             <div style={styles.retro} className="retro">
                 <header />
                 <main>
-                    <div style={styles.missions}>
-                        {
-                            missions.map(mission => {
-                                const imageUrl = getCurrentHeroImage(mission);
-                                const script = getRetroScript(mission);
-                                return (
-                                    <img key={mission} style={styles.mission}
-                                         src={imageUrl}
-                                         onClick={this.onMissionClick(script)}
-                                    />
-                                );
-                            })
-                        }
+                    <div style={styles.missionsContainer}>
+                        <div style={styles.missions}>
+                            {
+                                missions.map(mission => {
+                                    const imageUrl = getCurrentHeroImage(mission);
+                                    const script = getRetroScript(mission);
+                                    return (
+                                        <img key={mission} style={styles.mission}
+                                             src={imageUrl}
+                                             onClick={this.onMissionClick(script)}
+                                        />
+                                    );
+                                })
+                            }
+                        </div>
                     </div>
-                    <div>
-                        {this.state.script}
+                    <div style={styles.text}>
+                        {this.state.script || '(Tap on each thumbnail to review your journey)'}
                     </div>
                 </main>
+                <footer style={styles.footer}>
+                    <button style={styles.button} onClick={this.onReplayClick}>Replay</button>
+                </footer>
             </div>
         );
     }
 }
 
-export default Retro;
+export default withRouter(Retro);
