ba44beebc59361564b357bac68ca1a90238e257a
Weili Dai <weili.dai@turner.com>
2017-07-15 16:22:51 +0100

    Retro lightbox

--- src/components/retro/index.jsx
+++ src/components/retro/index.jsx
@@ -5,9 +5,9 @@ import {getRetroScript, getFrameHeroImage, deduplicate} from '../../data/states'
 const styles = {
     missions: {
         display: 'flex',
-        width: '90%',
+        width: '100%',
         flexWrap: 'wrap',
-        margin: '10px auto'
+        margin: '0px auto'
     },
     mission: {
         width: '25%',
@@ -20,7 +20,6 @@ const styles = {
         height: '100%'
     },
     text: {
-        paddingTop: '10px',
         margin: '0 auto',
         width: '310px',
         height: '48px',
@@ -33,7 +32,7 @@ const styles = {
     footer: {
         display: 'flex',
         justifyContent: 'center',
-        paddingTop: '10px'
+        paddingTop: '0px'
     },
     button: {
         fontSize: '15px',
@@ -46,15 +45,19 @@ class Retro extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            script: ''
+            script: '',
+            selectedImage: '',
+            showLightbox: false
         };
 
         this.onMissionClick = this.onMissionClick.bind(this);
         this.onReplayClick = this.onReplayClick.bind(this);
     }
 
-    onMissionClick(script) {
-        return () => this.setState({script});
+    onMissionClick(script, selectedImage) {
+        return () => {
+            this.setState({script, selectedImage, showLightbox: true})
+        };
     }
 
     onReplayClick() {
@@ -68,24 +71,31 @@ class Retro extends React.Component {
         return (
             <div style={styles.retro} className="retro">
                 <main>
-                        <div style={styles.missions}>
-                            {
-                                deduped.map(mission => {
-                                    const imageUrl = getFrameHeroImage(mission);
-                                    const script = getRetroScript(mission);
-                                    return (
-                                        <div key={mission} style={styles.mission}>
-                                            <img src={imageUrl} style={styles.image}
-                                                 onClick={this.onMissionClick(script)}/>
-                                        </div>
+                    {this.state.showLightbox && <div className="retro__mask" onClick={() => {this.setState({showLightbox: false})}}></div>}
+                    {this.state.showLightbox && <div className="retro__lightbox">
+                            <img className="retro__lightbox-close"
+                                 onClick={() => {this.setState({showLightbox: false})}}
+                                 src="/cross.png"
+                            />
+                            <img className="retro__lightbox-thumbnail" src={this.state.selectedImage} />
+                            <div className="retro__lightbox-caption">{this.state.script}</div>
+                    </div>}
+                    <div style={styles.missions}>
+                        {
+                            deduped.map(mission => {
+                                const imageUrl = getFrameHeroImage(mission);
+                                const script = getRetroScript(mission);
+                                return (
+                                    <div key={mission} style={styles.mission}>
+                                        <img src={imageUrl} style={styles.image}
+                                             onClick={this.onMissionClick(script, imageUrl)}/>
+                                    </div>
 
-                                    );
-                                })
-                            }
-                        </div>
-                    <div style={styles.text}>
-                        {this.state.script || '(Tap on each thumbnail to review your journey) (Tap on each thumbnail to review your journey)'}
+                                );
+                            })
+                        }
                     </div>
+                    <div style={styles.text}>(Tap on each thumbnail to review your journey)</div>
                 </main>
                 <footer style={styles.footer}>
                     <button style={styles.button} onClick={this.onReplayClick}>Replay</button>
